ddbdf39b70c23fb803480c3f1b5fa9c68a63de51
oussema <oussema.hamdi.97.pro@gmail.com>
2024-10-31 15:38:10 +0100

    Add Recipes POJO

---
+++ src/main/java/oussema/pojoclasses/RecipePOJO/Recipe.java
@@ -0,0 +1,26 @@
+package oussema.pojoclasses.RecipePOJO;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Recipe {
+    private int id;
+    private String name;
+    private List<String> ingredients;
+    private List<String> instructions;
+    private int prepTimeMinute;
+    private int cookTimeMinutes;
+    private int servings;
+    private String difficulty;
+    private String cuisine;
+    private int caloriesPerServing;
+    private List<String> tags;
+    private int userId;
+    private String image;
+    private double rating;
+    private int reviewCount;
+    private List<String> mealtype;
+}
